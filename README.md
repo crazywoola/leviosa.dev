b82ecda5a419598405ffbf99add24ef2afe70ce2
crazywoola <427733928@qq.com>
2023-12-29 10:48:58 +0800

    feat: example db usage

--- README.md
+++ README.md
@@ -4,5 +4,10 @@ npm run dev
 ```
 
 ```
+npx wrangler d1 execute <DATABASE_NAME> --local --file=./db/migrations/1.sql
+npx wrangler d1 execute <DATABASE_NAME> --local --command="SELECT * FROM Users"
+```
+
+```
 npm run deploy
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+type Bindings = {
+  DB: D1Database;
+};
 
-app.get('/', (c) => c.text('Hello Hono!'))
+const app = new Hono<{ Bindings: Bindings }>();
+app.get("/", (c) => c.text("Hello Hono!"));
+app.get("/about", (c) => c.text("About Hono!"));
+app.get("/users", async (c) => {
+  try {
+    let { results } = await c.env.DB.prepare("SELECT * FROM users").all()
+    return c.json(results)
+  } catch (e) {
+    return c.json({err: e}, 500)
+  }
+});
 
-export default app
+export default app;
