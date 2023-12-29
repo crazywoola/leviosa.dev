ae82d44b06871a32391457725e9f7e3fd841f4c5
crazywoola <427733928@qq.com>
2023-12-29 14:44:52 +0800

    fix: prod path

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import { serveStatic } from "hono/cloudflare-workers";
 import users from "./users";
 const app = new Hono();
 
-app.use("/favicon.ico", serveStatic({ path: "./favicon.png" }));
-app.use("/", serveStatic({ root: "./" }));
+app.use("/*", serveStatic({ root: "./" }));
 
 app.route("/users", users);
 
