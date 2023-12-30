f09c19c3c2a8750f5b402bb846913c17bcce46ef
crazywoola <427733928@qq.com>
2023-12-31 00:11:03 +0800

    feat: add btn

--- src/client/index.js
+++ src/client/index.js
@@ -3,19 +3,22 @@ import { Application, Controller } from "@hotwired/stimulus";
 window.Stimulus = Application.start();
 
 class OpeningController extends Controller {
-  static targets = ['title', 'description'];
+  static targets = ['title', 'subtitle', 'description'];
   static values = {
     title: String,
+    subtitle: String,
     description: String
   };
 
   connect() {
     this.animateElement(this.titleTarget, this.titleValue)
-        .then(() => this.animateElement(this.descriptionTarget, this.descriptionValue));
+        .then(() => this.animateElement(this.subtitleTarget, this.subtitleValue))
+        .then(() => this.animateElement(this.descriptionTarget, this.descriptionValue));   
   }
 
   animateElement(element, value) {
-    const duration = value.length / 20 * 1000; // Convert seconds to milliseconds
+    // const duration = value.length / 20 * 1000; // Convert seconds to milliseconds
+    const duration = 0
     element.innerHTML = value;
     element.style.width = `${value.length}ch`;
     element.style.animation = `typing ${duration / 1000}s steps(${value.length}), .5s step-end infinite alternate`;
@@ -26,4 +29,16 @@ class OpeningController extends Controller {
   }
 }
 
+class InfoController extends Controller {
+  static targets = ['btn'];
+  connect() {
+  }
+  toggle(){
+    this.btnTarget.classList.toggle('shadow-lg');
+    this.btnTarget.classList.toggle('rounded-lg');
+    this.btnTarget.classList.toggle('bg-purple-400');
+  }
+}
+
 window.Stimulus.register("opening", OpeningController);
+window.Stimulus.register("info", InfoController);
