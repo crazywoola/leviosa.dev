a883f61ba52cc8dc39f70c6ff21a216e27fc2165
crazywoola <427733928@qq.com>
2024-01-03 14:21:42 +0800

    feat: ssr icons

--- src/client/index.js
+++ src/client/index.js
@@ -3,68 +3,65 @@ import { Application, Controller } from "@hotwired/stimulus";
 window.Stimulus = Application.start();
 
 class OpeningController extends Controller {
-  static classes = ['start'];
-  static targets = ['title', 'subtitle', 'description', 'btn'];
+  static classes = ["start"];
+  static targets = ["title", "subtitle", "description", "btn"];
   static values = {
     title: String,
     subtitle: String,
-    description: String
+    description: String,
   };
 
   connect() {
     this.animateElement(this.titleTarget, this.titleValue)
       .then(() => this.animateElement(this.subtitleTarget, this.subtitleValue))
-      .then(() => this.animateElement(this.descriptionTarget, this.descriptionValue));
+      .then(() =>
+        this.animateElement(this.descriptionTarget, this.descriptionValue)
+      );
   }
 
   animateElement(element, value) {
     // const duration = value.length / 20 * 1000; // Convert seconds to milliseconds
-    const duration = 0
+    const duration = 0;
     element.innerHTML = value;
     element.style.width = `${value.length}ch`;
-    element.style.animation = `typing ${duration / 1000}s steps(${value.length}), .5s step-end infinite alternate`;
+    element.style.animation = `typing ${duration / 1000}s steps(${
+      value.length
+    }), .5s step-end infinite alternate`;
 
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(resolve, duration);
     });
   }
 
   start() {
     this.element.classList.toggle(this.startClass);
-    this.subtitleTarget.classList.toggle('hidden');
+    this.subtitleTarget.classList.toggle("hidden");
   }
 }
 
 class InfoController extends Controller {
-  static targets = ['btn', 'modal'];
+  static targets = ["icons"];
+
+  static values = {
+    icons: Array,
+    url: String,
+  };
   connect() {
-  }
-  toggle() {
-    this.btnTarget.classList.toggle('shadow-lg');
-    this.btnTarget.classList.toggle('rounded-lg');
-    this.btnTarget.classList.toggle('bg-purple-200');
-    this.modalTarget.classList.toggle('hidden');
-  }
-  close() {
-    this.btnTarget.classList.remove('shadow-lg');
-    this.btnTarget.classList.remove('rounded-lg');
-    this.btnTarget.classList.remove('bg-purple-200');
-    this.modalTarget.classList.add('hidden');
+    console.log(this.iconsValue);
+    console.log(this.urlValue);
+    this.iconsValue.forEach((icon) => {
+      const url = `${this.urlValue}/${icon}`;
+      fetch(url).then((resp) => resp.text()).then((data) => {
+        this.iconsTarget.innerHTML += data;
+      });
+    });
   }
 }
 
 class MainController extends Controller {
-  static values = {
-    url: String
-  }
+  static values = {};
 
-  connect() {
-    fetch(this.urlValue).then(response => {
-      response.json().then(data => {
-        console.log(data);
-      });
-    });
-  }
+  connect() {}
 }
 
 window.Stimulus.register("opening", OpeningController);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/cloudflare-workers";
 import users from "./users";
+import icons from "./icons";
+
 const app = new Hono();
 
 // Serve static files
@@ -12,5 +14,6 @@ app.get('/favicon.png', serveStatic({ path: './favicon.png' }))
 app.get('/',serveStatic({ path: './index.html' }))
 app.get('/about',serveStatic({ path: './about.html' }))
 app.route("/users", users);
+app.route("/icons", icons);
 
 export default app;
