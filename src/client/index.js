d571d19fda9c4e7817c559b064a3e479913c0ead
crazywoola <427733928@qq.com>
2024-01-04 15:31:52 +0800

    feat: add more things

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,12 @@
 import { Application, Controller } from "@hotwired/stimulus";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import dayjs from "dayjs";
+
+// dayjs config
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.guess();
 
 window.Stimulus = Application.start();
 
@@ -47,21 +55,61 @@ class InfoController extends Controller {
     url: String,
   };
   connect() {
-    console.log(this.iconsValue);
-    console.log(this.urlValue);
     this.iconsValue.forEach((icon) => {
       const url = `${this.urlValue}/${icon}`;
-      fetch(url).then((resp) => resp.text()).then((data) => {
-        this.iconsTarget.innerHTML += data;
-      });
+      fetch(url)
+        .then((resp) => resp.text())
+        .then((data) => {
+          this.iconsTarget.innerHTML += data;
+        });
     });
   }
 }
 
 class MainController extends Controller {
+  static targets = ["calendar", 'calendarMonth'];
   static values = {};
 
-  connect() {}
+  connect() {
+    const today = dayjs();
+    const firstDayOfMonth = today.startOf("month");
+    const daysInMonth = today.daysInMonth();
+    const paddingDaysCount = firstDayOfMonth.day();
+    const weekdayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    // Set the calendar title
+    this.calendarMonthTarget.innerHTML = today.format("MMMM YYYY");
+    // Create an array representing both padding days and actual days
+    const calendarDays = [
+      ...weekdayNames, // Weekday names
+      ...Array(paddingDaysCount).fill(""), // Padding days
+      ...Array.from({ length: daysInMonth }, (_, i) => i + 1), // Actual days
+    ];
+    // Use map to iterate over the array and create calendar elements
+    calendarDays.map((day) => this.createCalendarElement(day));
+  }
+
+  show() {
+    console.log("show");
+  }
+  createCalendarElement(content) {
+    const elem = document.createElement("div");
+    // add data-action="mouseover->main#open"
+    elem.setAttribute("data-action", "mouseover->main#show");
+    elem.classList.add(
+      "border",
+      "border-purple-600",
+      "flex",
+      "justify-center",
+      "items-center",
+      "text-purple-600",
+      "cursor-pointer"
+    );
+    // hover:bg-purple-600 hover:text-white
+    elem.classList.add("hover:bg-purple-600", "hover:text-white");
+    elem.innerHTML = content;
+    this.calendarTarget.appendChild(elem);
+  }
 }
 
 window.Stimulus.register("opening", OpeningController);
