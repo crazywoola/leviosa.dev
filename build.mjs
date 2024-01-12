c321017daf2ae45b1a1f432b411a66002de44a9d
crazywoola <427733928@qq.com>
2024-01-12 23:37:28 +0800

    feat: use hotwire/stimulus

--- build.mjs
+++ build.mjs
@@ -1,24 +1,19 @@
 import * as esbuild from 'esbuild'
 
+const config = {
+  entryPoints: ['src/index.js'],
+  bundle: true,
+  minify: false,
+  sourcemap: false,
+  outfile: 'assets/js/bundle.js',
+  jsx: 'automatic',
+};
+
 // Dev
-console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'dev') {
-  const ctx = await esbuild.context({
-    entryPoints: ['src/index.tsx'],
-    bundle: true,
-    minify: false,
-    sourcemap: false,
-    outfile: 'assets/js/bundle.js',
-  });
+  const ctx = await esbuild.context(config);
   await ctx.watch();
-  console.log('Watching for changes...');
 } else {
   // Production
-  await esbuild.build({
-    entryPoints: ['src/index.tsx'],
-    bundle: true,
-    minify: true,
-    sourcemap: false,
-    outfile: 'assets/js/bundle.js',
-  });
+  await esbuild.build(...config, { minify: true});
 }
