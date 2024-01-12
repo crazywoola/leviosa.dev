47e576906b5a4594bfb751e866afe2f7ec5f5152
crazywoola <427733928@qq.com>
2024-01-12 23:16:27 +0800

    init: project

--- build.mjs
+++ build.mjs
@@ -1,9 +1,24 @@
 import * as esbuild from 'esbuild'
 
-await esbuild.build({
-  entryPoints: ['src/index.tsx'],
-  bundle: true,
-  minify: true,
-  sourcemap: false,
-  outfile: 'assets/js/bundle.js',
-});
+// Dev
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'dev') {
+  const ctx = await esbuild.context({
+    entryPoints: ['src/index.tsx'],
+    bundle: true,
+    minify: false,
+    sourcemap: false,
+    outfile: 'assets/js/bundle.js',
+  });
+  await ctx.watch();
+  console.log('Watching for changes...');
+} else {
+  // Production
+  await esbuild.build({
+    entryPoints: ['src/index.tsx'],
+    bundle: true,
+    minify: true,
+    sourcemap: false,
+    outfile: 'assets/js/bundle.js',
+  });
+}
