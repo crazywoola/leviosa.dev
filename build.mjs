e39d51bbdf47f6ad44a22a1146eae0b476343441
crazywoola <427733928@qq.com>
2024-01-12 23:45:25 +0800

    feat: update things

--- build.mjs
+++ build.mjs
@@ -3,17 +3,17 @@ import * as esbuild from 'esbuild'
 const config = {
   entryPoints: ['src/index.js'],
   bundle: true,
-  minify: false,
+  minify: process.env.NODE_ENV === 'development' ? false : true,
   sourcemap: false,
   outfile: 'assets/js/bundle.js',
   jsx: 'automatic',
 };
 
 // Dev
-if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'development') {
   const ctx = await esbuild.context(config);
   await ctx.watch();
 } else {
   // Production
-  await esbuild.build(...config, { minify: true});
+  await esbuild.build(config);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./assets/**/*.{html,js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
